164e01297a2e28cc7f2b6cab24510ee955135243
FelipeThim <162934100+FelipeThim@users.noreply.github.com>
2026-02-26 23:39:31 -0300

    Update Desafio3.js

--- Desafio3.js
+++ Desafio3.js
@@ -1,1 +1,23 @@
+class Herói{
+  constructor(nome, idade, tipo){
+  this.nome = nome
+  this.idade = idade
+  this.tipo = tipo
+}
+  atacar(){
+    let ataque
+      if (this.tipo === "mago") {
+      ataque = "magia"
+    } else if (this.tipo === "guerreiro") {
+      ataque = "espada"
+    } else if (this.tipo === "monge") {
+      ataque = "artes marciais"
+    } else if (this.tipo === "ninja") {
+      ataque = "shuriken"
+      }
+    console.log(`O herói de nome ${this.nome}, ${this.idade} anos, de tipo ${this.tipo} atacou usando ${ataque}`)
+  }
+}
+    const Herói1 = new Herói("Felipe", 15, "ninja")
+        Herói1.atacar()
 
